f89ee7dfb0452396a0776e59dd318af0af751b46
Ravi <ravivyas922@gmail.com>
2023-01-02 15:40:55 -0500

    commit tree 157ec86a2e1627dccfa38ab363875b746f85e07f

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function () {
+    const e = document.getElementById("text");
+    e.textContent = "This is really cool!";
+}
+)
